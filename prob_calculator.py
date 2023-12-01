26eab05939a6b164db6ba308664599b6c61cfc5b
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-01 18:18:52 -0500

    Implemented the experiment function to calculate probabilities.

--- prob_calculator.py
+++ prob_calculator.py
@@ -15,3 +15,16 @@ class Hat:
     def draw(self, number):
         number = min(number, len(self.contents))  # Adjust the number if it exceeds the available balls.
         return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(number)]  # Randomly draw and remove balls from the hat.
+
+# The experiment function: Conducts a specified number of experiments to calculate the probability.
+# This function takes a Hat object, an expected ball distribution, number of balls to draw, and number of experiments.
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    success_count = 0  # Initialize a count of successful experiments.
+    for _ in range(num_experiments):
+        # Create a copy of the hat for each experiment to preserve original contents.
+        experiment_hat = Hat(**{color: hat.contents.count(color) for color in set(hat.contents)})
+        drawn_balls = experiment_hat.draw(num_balls_drawn)  # Draw balls from the hat.
+        # Check if the drawn balls meet the expected distribution.
+        if all(drawn_balls.count(color) >= count for color, count in expected_balls.items()):
+            success_count += 1  # Increment the success count if the experiment meets expectations.
+    return success_count / num_experiments  # Return the probability based on the number of successful experiments.
